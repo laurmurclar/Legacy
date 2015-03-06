2e08b920846397f823082590b6439cf4cdbf9213
laurmurclar <murphl26@tcd.ie>
2015-03-06 13:29:17 +0000

    Family Screen has 3 checked checkboxes, which use an array to be initialised & set.

--- app/src/main/java/com/bakincakes/legacy/FamilyActivity.java
+++ app/src/main/java/com/bakincakes/legacy/FamilyActivity.java
@@ -2,24 +2,31 @@ package com.bakincakes.legacy;
 
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class FamilyActivity extends BaseActivity {
     private String[] navMenuTitles;
     private TypedArray navMenuIcons;
+    private int NCHECKBOX = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
         navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load
-        // titles
-        // from
-        // strings.xml
+        // titles from strings.xml
 
         navMenuIcons = getResources()
-                .obtainTypedArray(R.array.nav_drawer_icons);// load icons from
-        // strings.xml
+                .obtainTypedArray(R.array.nav_drawer_icons);// load icons from strings.xml
 
         set(navMenuTitles, navMenuIcons);
+        //set the checkboxes
+        String cbKey = "fam_cb";
+        CheckBox[] checkboxes = new CheckBox[NCHECKBOX];
+        for (int i = 0; i < checkboxes.length; i++){
+            checkboxes[i] = (CheckBox) findViewById(getResources().getIdentifier(cbKey+i, "id",getPackageName()));
+            checkboxes[i].setChecked(true);
+        }
+        //load each saved preference
     }
 }
