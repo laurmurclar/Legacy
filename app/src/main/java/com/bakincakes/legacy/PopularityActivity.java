c4d28053144d7d1d49b1cf174df15cf777be15af
laurmurclar <murphl26@tcd.ie>
2015-03-09 11:33:34 +0000

    Made popularity work with nav drawer

--- app/src/main/java/com/bakincakes/legacy/PopularityActivity.java
+++ app/src/main/java/com/bakincakes/legacy/PopularityActivity.java
@@ -1,17 +1,41 @@
 package com.bakincakes.legacy;
 
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
 
 
-public class PopularityActivity extends ActionBarActivity {
+public class PopularityActivity extends BaseActivity {
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+    private int NCHECKBOX = 10;
+    final CheckBox[] checkboxes = new CheckBox[NCHECKBOX];
+    int medalPoints, nGold, nSilver, nBronze;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popularity);
+
+        /* Set up Nav Bar */
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load
+        // titles from strings.xml
+
+        navMenuIcons = getResources()
+                .obtainTypedArray(R.array.nav_drawer_icons);// load icons from strings.xml
+
+        set(navMenuTitles, navMenuIcons);
+
+        nGold = getIntPref("gold_medals");
+        nSilver = getIntPref("silver_medals");
+        nBronze = getIntPref("bronze_medals");
+        medalPoints = totalMedalPoints();
     }
 
 
@@ -36,4 +60,27 @@ public class PopularityActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private int getIntPref(String key){
+       SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+       return  sharedPreferences.getInt(key, 0);
+    }
+
+    private void saveIntPref(String key, int value){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    private int totalMedalPoints(){
+        return nBronze+(2*nSilver)+(3*nGold);
+    }
+
+    /**
+     * on button click
+     *
+     * increase/decrease the appropriate medal count, and then save it
+     * then update medalPoints
+     */
 }
